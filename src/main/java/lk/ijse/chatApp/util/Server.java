959a4098ef1f769a4db9d6b4c43ebc1480663287
YurajMalinda <yurajmalinda123.com>
2023-07-05 21:30:11 +0530

    Add codes to server class. The server class connects the clients.

--- src/main/java/lk/ijse/chatApp/util/Server.java
+++ src/main/java/lk/ijse/chatApp/util/Server.java
@@ -1,5 +1,24 @@
 package lk.ijse.chatApp.util;
 
-public class Server {
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
 
+public class Server {
+    private static ArrayList<Handler> clients = new ArrayList<Handler>();
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(4000);
+        Socket accept;
+
+        while (true){
+            System.out.println("Waiting for Clients...");
+            accept = serverSocket.accept();
+            System.out.println("Client Connected...");
+            Handler clientThread = new Handler(accept, clients);
+            clients.add(clientThread);
+            clientThread.start();
+        }
+    }
 }
