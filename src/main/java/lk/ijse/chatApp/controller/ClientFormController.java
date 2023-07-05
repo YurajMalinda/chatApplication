54e61a5dc1e55c5a2e61942e524b652e5fa47645
YurajMalinda <yurajmalinda123.com>
2023-07-05 19:50:15 +0530

    Modify the client controller.

--- src/main/java/lk/ijse/chatApp/controller/ClientFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/ClientFormController.java
@@ -1,4 +1,81 @@
 package lk.ijse.chatApp.controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+
 public class ClientFormController {
+    public AnchorPane client;
+    public Label lblName;
+    public ScrollPane txtChat;
+    public VBox v_box;
+    public TextField txtMessage;
+    public AnchorPane emoji_pane;
+
+    public void txtMessageOnAction(ActionEvent actionEvent) {
+    }
+
+    public void mouseOnClickHideEmoji(MouseEvent mouseEvent) {
+    }
+
+    public void btnSendOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void btnCameraOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void emojiPaneOnAction(MouseEvent mouseEvent) {
+    }
+
+    public void sad(MouseEvent mouseEvent) {
+    }
+
+    public void lot_sad(MouseEvent mouseEvent) {
+    }
+
+    public void money(MouseEvent mouseEvent) {
+    }
+
+    public void love(MouseEvent mouseEvent) {
+    }
+
+    public void green_sad(MouseEvent mouseEvent) {
+    }
+
+    public void smile_one_eyy(MouseEvent mouseEvent) {
+    }
+
+    public void cry_yes(MouseEvent mouseEvent) {
+    }
+
+    public void cry_head(MouseEvent mouseEvent) {
+    }
+
+    public void real_amile(MouseEvent mouseEvent) {
+    }
+
+    public void tuin(MouseEvent mouseEvent) {
+    }
+
+    public void woow(MouseEvent mouseEvent) {
+    }
+
+    public void smile_normal(MouseEvent mouseEvent) {
+    }
+
+    public void large_smile(MouseEvent mouseEvent) {
+    }
+
+    public void small_smile(MouseEvent mouseEvent) {
+    }
+
+    public void tong_smile(MouseEvent mouseEvent) {
+    }
+
+    public void btnEmojiOnAction(MouseEvent mouseEvent) {
+    }
 }
