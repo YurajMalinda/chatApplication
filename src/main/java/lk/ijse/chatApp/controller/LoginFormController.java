78be9ac55165b8496be24d58dd1cc53cb49daf79
YurajMalinda <yurajmalinda123.com>
2023-07-05 20:15:04 +0530

    Modify the login form.

--- src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
@@ -1,7 +1,12 @@
 package lk.ijse.chatApp.controller;
 
+import javafx.event.ActionEvent;
 import javafx.scene.control.TextField;
 
 public class LoginFormController {
-    public TextField txtMessage;
+    public TextField txtName;
+
+
+    public void txtNameOnAction(ActionEvent actionEvent) {
+    }
 }
