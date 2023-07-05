21f626b82c4c9e23afc1277b1aa54f6dc98f1c03
YurajMalinda <yurajmalinda123.com>
2023-07-05 20:21:02 +0530

    Add codes to Login Controller.

--- src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
+++ src/main/java/lk/ijse/chatApp/controller/LoginFormController.java
@@ -1,12 +1,24 @@
 package lk.ijse.chatApp.controller;
 
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class LoginFormController {
     public TextField txtName;
+    static String userName;
 
-
-    public void txtNameOnAction(ActionEvent actionEvent) {
+    public void txtNameOnAction(ActionEvent actionEvent) throws IOException {
+        userName = txtName.getText();
+        txtName.clear();
+        Stage stage = new Stage();
+        stage.setScene(new Scene(FXMLLoader.load(LoginFormController.class.getResource("/view/client_form.fxml"))));
+        stage.close();
+        stage.centerOnScreen();
+        stage.show();
     }
 }
